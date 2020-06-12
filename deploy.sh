c9646f621c41a25760a49104b67e8941df671ed0
ZengMin <550609063@qq.com>
2020-06-12 18:29:39 +0800

    commit master xxx

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ git checkout gh-pages    # 检出pagss分支，首次命令git checkout -b gh-pa
 git reset --hard master  # 将gh-pages分支重置为与master分支内容一致
 
 # 在gp-pages分支下，保留_book和node_modules目录和隐藏文件，其余内容全部删除
-for file in `ls $1`  #注意此处这是两个反引号，表示运行系统命令,下面文件处理的地方也需要加``或者用$()
+for file in `ls $1`  #注意此处这是两个反引号，表示运行系统命令。
 do
     if [ $file = "_book" ] || [ $file = "node_modules" ]
     then
@@ -26,4 +26,5 @@ git commit -a -m "commit gh-pages xxx"  # 提交到本地仓库
 # 发布所有分支
 git push -f --all origin    # 推送回远程代码仓库，如果没有更新，手动更新码云pages服务。
 
-git checkout -f master      # 返回master分支
+# 返回master分支
+git checkout -f master      
